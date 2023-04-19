257dd5a2902506130f6db16ead70ef59e4d2c0b2
Mateus Castro <matcastro2004@yahoo.com.br>
2023-04-19 19:46:52 -0300

    Include first raw controllers

---
+++ Personal.Control/Personal.Control/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Personal.Control.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// Retrive data from a registered user
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Register a user to use the system
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpPut]
+        public IActionResult Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Resets a user password
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpPost]
+        [Route("password/reset")]
+        public IActionResult ResetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes the user from the system
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrive user permissions
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet]
+        [Route("{id}/permissions")]
+        public IActionResult GetPermissions(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Update data of an user
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{id}")]
+        public IActionResult Update(string id)
+        {
+            return Ok(id);
+        }
+    }
+}
