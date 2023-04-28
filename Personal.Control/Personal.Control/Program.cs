909df4eec9c92b6194594cc9acec0b9fae9942ea
Mateus Castro <matcastro2004@yahoo.com.br>
2023-04-28 17:28:16 -0300

    Move dependency injection of repositories to repositories project

--- Personal.Control/Personal.Control/Program.cs
+++ Personal.Control/Personal.Control/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Personal.Control.Configs;
-using Personal.Control.Repositories.Contexts;
+using Personal.Control.Repositories.DI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,15 +8,7 @@ var configuration = builder.Configuration.GetSection(nameof(Config)).Get<Config>
 // Add services to the container.
 builder.Services.Configure<Config>(builder.Configuration.GetSection(nameof(Config)));
 
-var databaseServer= Environment.GetEnvironmentVariable("MYSQL_DATABASE_SERVER");
-var databaseUser= Environment.GetEnvironmentVariable("MYSQL_DATABASE_USER");
-var databasePassword = Environment.GetEnvironmentVariable("MYSQL_DATABASE_PASSWORD");
-var connectionString = builder.Configuration.GetConnectionString("Control")!;
-connectionString = string.Format(connectionString, databaseServer, databaseUser, databasePassword);
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-);
+builder.Services.AddRepositories(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
