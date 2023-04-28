e4bf33e91bfa8f65da46bfaff55a32b0a2e4c0c8
Mateus Castro <matcastro2004@yahoo.com.br>
2023-04-27 23:03:03 -0300

    Create first database connection with dbcontext and environment variables

---
+++ Personal.Control/Personal.Control.Repositories/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal.Control.Repositories.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string email, string password)
+        {
+            Id = Guid.NewGuid().ToString();
+            Email = email;
+            Password = password;
+        }
+    }
+}
--- Personal.Control/Personal.Control/Program.cs
+++ Personal.Control/Personal.Control/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Personal.Control.Configs;
+using Personal.Control.Repositories.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration.GetSection(nameof(Config)).Get<Config>();
+
 // Add services to the container.
+builder.Services.Configure<Config>(builder.Configuration.GetSection(nameof(Config)));
+
+var databaseServer= Environment.GetEnvironmentVariable("MYSQL_DATABASE_SERVER");
+var databaseUser= Environment.GetEnvironmentVariable("MYSQL_DATABASE_USER");
+var databasePassword = Environment.GetEnvironmentVariable("MYSQL_DATABASE_PASSWORD");
+var connectionString = builder.Configuration.GetConnectionString("Control")!;
+connectionString = string.Format(connectionString, databaseServer, databaseUser, databasePassword);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
