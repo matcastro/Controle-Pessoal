5d3c0076ff44d12641015e95213103f393ef04aa
Mateus Castro <matcastro2004@yahoo.com.br>
2023-05-07 19:52:31 -0300

    Trim email and encrypt password

---
+++ Personal.Control/Personal.Control.Configs/Criptography/PasswordEncryptor.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Personal.Control.Utils.Criptography
+{
+    public static class PasswordEncryptor
+    {
+        const int keySize = 64;
+        const int iterations = 350000;
+        private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+
+        public static string Encrypt(string password, byte[] salt)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                salt,
+                iterations,
+                hashAlgorithm,
+                keySize);
+
+            return Convert.ToHexString(hash);
+        }
+    }
+}
--- Personal.Control/Personal.Control/Models/Requests/UserRequest.cs
+++ Personal.Control/Personal.Control/Models/Requests/UserRequest.cs
@@ -2,7 +2,18 @@
 {
     public class UserRequest
     {
-        public string Email { get; set; } = string.Empty;
+        private string _email = string.Empty;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value.Trim();
+            }
+        }
         public string Password { get; set; } = string.Empty;
     }
 }
