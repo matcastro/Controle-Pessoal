57818815a2008da30d2e71c9c2d7c90de884d129
Mateus Castro <matcastro2004@yahoo.com.br>
2023-08-02 20:52:33 -0300

    Create test for repositories

---
+++ test/Personal.Control.Repositories.Tests/TestDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Personal.Control.Repositories.Contexts;
+
+namespace Personal.Control.Repositories.Tests
+{
+    public class TestDbContextFactory : IDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase("test");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
