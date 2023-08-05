4cf5da6be5a8aecb1606a3745e1d5f36cf9309a0
Mateus Castro <matcastro2004@yahoo.com.br>
2023-08-04 22:20:48 -0300

    Improve code coverage

---
+++ test/Personal.Control.Repositories.Tests/Utils/DbSetExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using Personal.Control.Repositories.Models;
+using Personal.Control.Repositories.Utils;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Personal.Control.Repositories.Tests.Utils
+{
+    public class DbSetExtensionsTests
+    {
+        private readonly Fixture _fixture;
+        private readonly TestDbContextFactory _dbContextFactory;
+        public DbSetExtensionsTests()
+        {
+            _fixture = new Fixture();
+            _dbContextFactory = new TestDbContextFactory();
+        }
+
+        [Fact]
+        public void AddIfNotExists_WhenPredicateIsNull_ShouldAdd()
+        {
+            var user = _fixture.Create<User>();
+            using var context = _dbContextFactory.CreateDbContext();
+            context.Users.AddIfNotExists(user, out var success, null);
+            context.SaveChanges();
+            context.Users.AddIfNotExists(user, out success, null);
+
+            Assert.True(success);
+        }
+
+        [Fact]
+        public void AddIfNotExists_WhenPredicateMatches_ShouldNotAdd()
+        {
+            var user = _fixture.Create<User>();
+            using var context = _dbContextFactory.CreateDbContext();
+
+            Expression<Func<User, bool>> predicate = u => u.Id == user.Id;
+            context.Users.AddIfNotExists(user, out var success, predicate);
+            context.SaveChanges();
+            context.Users.AddIfNotExists(user, out success, predicate);
+
+            Assert.False(success);
+        }
+    }
+}
---
+++ test/Personal.Control.Tests/Middlewares/ExceptionHandlerMiddlewareTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Personal.Control.Middlewares;
+using Personal.Control.Utils.Exceptions;
+using System.Net;
+using Xunit;
+
+namespace Personal.Control.Tests.Middlewares
+{
+    public class ExceptionHandlerMiddlewareTests
+    {
+        private readonly ExceptionHandlerMiddleware _middleware;
+        private readonly Mock<RequestDelegate> _requestDelegate;
+
+        public ExceptionHandlerMiddlewareTests()
+        {
+            _requestDelegate = new Mock<RequestDelegate>();
+            _middleware = new ExceptionHandlerMiddleware(_requestDelegate.Object);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenNextThrowsArgumentException_ShouldSetResponseCodeTo400()
+        {
+            var ctx = new DefaultHttpContext();
+            _requestDelegate.Setup(next => next(ctx)).ThrowsAsync(new ArgumentException());
+
+            await _middleware.Invoke(ctx);
+
+            Assert.Equal((int) HttpStatusCode.BadRequest, ctx.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenNextThrowsDuplicatedEntityException_ShouldSetResponseCodeTo400()
+        {
+            var ctx = new DefaultHttpContext();
+            _requestDelegate.Setup(next => next(ctx)).ThrowsAsync(new DuplicatedEntityException());
+
+            await _middleware.Invoke(ctx);
+
+            Assert.Equal((int)HttpStatusCode.BadRequest, ctx.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenNextThrowsUnhandledExceptions_ShouldSetResponseCodeTo500()
+        {
+            var ctx = new DefaultHttpContext();
+            _requestDelegate.Setup(next => next(ctx)).ThrowsAsync(new Exception());
+
+            await _middleware.Invoke(ctx);
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, ctx.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenNextRunsNormally_ShouldSetResponseCodeTo200()
+        {
+            var ctx = new DefaultHttpContext();
+
+            await _middleware.Invoke(ctx);
+
+            Assert.Equal((int)HttpStatusCode.OK, ctx.Response.StatusCode);
+        }
+    }
+}
